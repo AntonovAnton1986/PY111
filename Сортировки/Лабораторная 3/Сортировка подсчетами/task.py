65b61c08ca5a2de16ca27cec926bb4db2c762df3
AntonovAnton1986 <antonovspb78@gmail.com>
2026-01-22 15:02:28 +0300

    LAB3

--- Сортировки/Лабораторная 3/Сортировка подсчетами/task.py
+++ Сортировки/Лабораторная 3/Сортировка подсчетами/task.py
@@ -13,3 +13,24 @@ def sort(container: Sequence[int]) -> Sequence[int]:
     :return: Отсортированный в порядке возрастания массив
     """
     ...  # TODO реализовать алгоритм сортировки подсчетами
+    if not container:  # Если массив пустой, возвращаем его как есть
+        return container
+    
+
+    max_val = max(container)
+    min_val = min(container)
+    
+   
+    count_size = max_val - min_val + 1
+    count = [0] * count_size
+    
+   
+    for num in container:
+        count[num - min_val] += 1
+    
+   
+    result = []
+    for i in range(count_size):
+        result.extend([i + min_val] * count[i])
+    
+    return result
