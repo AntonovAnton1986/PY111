085d8629beb612138853cf02db740f84ffeaa925
AntonovAnton1986 <antonovspb78@gmail.com>
2026-01-26 14:55:40 +0300

    LAB

--- Сортировки/Лабораторная 3/Сортировка пузырьком/task.py
+++ Сортировки/Лабораторная 3/Сортировка пузырьком/task.py
@@ -14,3 +14,14 @@ def sort(container: Sequence[int]) -> Sequence[int]:
     :return: Отсортированный в порядке возрастания массив
     """
     ...  # TODO реализовать алгоритм сортировки пузырьком
+
+
+    arr = list(container)
+    n = len(arr)
+
+    for i in range(n):
+        for j in range(n - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+
+    return arr
