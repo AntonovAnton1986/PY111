2873e934bc93d02387fb2521dd6bccbd9bb5a76d
AntonovAnton1986 <antonovspb78@gmail.com>
2026-01-26 14:59:35 +0300

    LAB4

--- Динамическое программирование/Лабораторная 4/Автомобиль. Количество маршрутов/task.py
+++ Динамическое программирование/Лабораторная 4/Автомобиль. Количество маршрутов/task.py
@@ -13,6 +13,24 @@ def car_paths(n: int, m: int) -> List[List[int]]:
     ...  # TODO решить задачу с помощью динамического программирования
 
 
+    # создаём таблицу n x m, заполненную нулями
+    dp = [[0] * m for _ in range(n)]
+
+    # стартовая клетка
+    dp[0][0] = 1
+
+    for i in range(n):
+        for j in range(m):
+            if i == 0 and j == 0:
+                continue
+
+            from_top = dp[i - 1][j] if i > 0 else 0
+            from_left = dp[i][j - 1] if j > 0 else 0
+            from_diag = dp[i - 1][j - 1] if i > 0 and j > 0 else 0
+
+            dp[i][j] = from_top + from_left + from_diag
+
+    return dp
 if __name__ == '__main__':
     paths = car_paths(4, 2)
     print(paths[-1][-1])  # 7
